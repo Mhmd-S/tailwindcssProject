28b1a88b068ce670c1662206ce5f661015a60521
Mhmd-S <65135074+Mhmd-S@users.noreply.github.com>
2023-02-22 22:07:17 +0800

    Fixed the post botton in the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
   return (
     <header className='w-4/5 grid grid-cols-[80%_10%_10%] border-b-4 mx-auto py-8 border-white '>
         <AiOutlineBug size={50} color={"white"}  className='mr-100'/>
-        <button className='justify-self-end text-white font-bold tracking-wider border-b-4 border-transparent hover:border-b-4 hover:border-orange-500 ease-in duration-300'>Posts</button>
+        <button className='relative justify-self-end text-white font-bold tracking-wider before:block before:absolute before:w-full  before:top-10 before:scale-x-0 before:origin-bottom-right before:h-[0.25rem] before:bg-orange-500 before:hover:scale-x-100 before:hover:origin-top-left before:transition-transform'>Posts</button>
         <button className='justify-self-end'><BsSun size={20} color={"white"} /></button>
     </header>
   )
